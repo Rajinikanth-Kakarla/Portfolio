9c666fc8849a900a96e1ee938072b25e5d4cbb2e
Rajinikanth Kakarla <rajinikanthkakarla.03@gmail.com>
2025-04-27 22:21:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// For future interactivity or form handling
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Your message has been sent!');
+});
