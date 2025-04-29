5ff04f61b791fb6d59aac447e8bd8dccf892da96
Rajinikanth Kakarla <rajinikanthkakarla.03@gmail.com>
2025-04-29 23:37:55 +0530

    Updated

--- script.js
+++
@@ -1,5 +0,0 @@
-// For future interactivity or form handling
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Your message has been sent!');
-});
